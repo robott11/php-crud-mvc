aebd6f4151a01bca541d2fd98b19319de792be0a
robott11 <contato.robott@gmail.com>
2022-03-09 10:11:24 -0300

    created public directory

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__."/includes/app.php";
+require __DIR__."/../includes/app.php";
 
 use \App\Http\Router;
 
@@ -7,10 +7,10 @@ use \App\Http\Router;
 $obRouter = new Router(URL);
 
 //INCLUDE THE PAGES ROUTES
-include __DIR__."/routes/pages.php";
+include __DIR__."/../routes/pages.php";
 
 //INCLUDE THE ADMIN ROUTES
-include __DIR__."/routes/admin.php";
+include __DIR__."/../routes/admin.php";
 
 //PRINT THE ROUTER RESPONSE
 $obRouter->run()
